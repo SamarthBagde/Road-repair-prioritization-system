c45c62fc25b2731190250a01a827aefa366d4387
SamarthBagde <141544073+SamarthBagde@users.noreply.github.com>
2024-02-13 16:18:57 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ADS-CP
+ Road repair prioritization system using Kruskal's algorithm
---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("ADS project");
+    return 0;
+}
